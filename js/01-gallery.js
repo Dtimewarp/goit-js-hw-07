bd08f052cbc5c153edad53c1acfaf1f40f126a54
Dtimewarp <dtimewarp@gmail.com>
2023-11-10 23:04:15 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+galleryList.addEventListener("click", handleClick);
+
+function createMarkup(array) {
+    return array.map(({preview, original, description}) => 
+    `<li class="gallery__item"">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+        </a>
+    </li>`
+    ).join("")
+}
+// console.log(createMarkup(galleryItems));
+
+function handleClick(event) { 
+    event.preventDefault();
+    if ( event.target === event.currentTarget) {
+        return;
+    }
+    // console.log("currentTarget", event.currentTarget);
+    // console.log("target", event.target);
+  
+    const currentImage = event.target;
+    const img =  currentImage.dataset.source;
+
+
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img class="gallery__image" src="${img}">
+    </div>
+`)
+instance.show()
+}
+
+
+
+
+
+
+
