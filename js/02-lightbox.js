5e8b615054d61c66d459c5cb1fd72710d68ef654
Dtimewarp <dtimewarp@gmail.com>
2023-11-11 13:55:09 +0200

    task1+task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+// Ініціалізація SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function createMarkup(array) {
+  return array.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+  ).join('');
+}
